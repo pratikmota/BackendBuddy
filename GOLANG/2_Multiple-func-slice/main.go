ea506fb918ca03789048eb379b6de97434b84be3
Pratik Mota <mota.pratik@gmail.com>
2023-01-01 12:48:01 +0530

    go work add and multiple func

--- GOLANG/2_Multiple-func-slice/main.go
+++ GOLANG/2_Multiple-func-slice/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+    funcs := []func() {
+        BackendBuddy1,
+        BackendBuddy2,
+        BackendBuddy3,
+    }
+
+    for _, f := range funcs{
+        f()
+    }
+
+}
+
+
+func BackendBuddy1() {
+    fmt.Println("Backend Buddy 1")
+}
+
+func BackendBuddy2() {
+    fmt.Println("Backend Buddy 2")
+}
+
+func BackendBuddy3() {
+    fmt.Println("Backend Buddy 3")
 }
